875286bd4a9f8abbfdca8efd490a55ee4ff6994c
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-09-18 01:10:22 +0900

    Rename

--- D3D12HelloTriangleWpfWinFormInterop/App.xaml.cs
+++ D3D12HelloTriangleWpfWinFormInterop/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace D3D12HelloTriangleWpf
+namespace D3D12HelloTriangleWpfWinFormInterop
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- D3D12HelloTriangleWpfWinFormInterop/MainWindow.xaml.cs
+++ D3D12HelloTriangleWpfWinFormInterop/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace D3D12HelloTriangleWpf
+namespace D3D12HelloTriangleWpfWinFormInterop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
