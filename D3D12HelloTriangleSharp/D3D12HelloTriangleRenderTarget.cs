1322bc5aa33fdd5a3041fadf282c88eb8963477f
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-09-22 01:56:32 +0900

    Add D3D12HelloTriangleRenderTarget, GraphicsDevice9

---
+++ D3D12HelloTriangleSharp/D3D12HelloTriangleRenderTarget.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DXGI = SharpDX.DXGI;
+using D3D12 = SharpDX.Direct3D12;
+
+namespace D3D12HelloTriangleSharp
+{
+    public sealed class D3D12HelloTriangleRenderTarget : IDisposable
+    {
+        private readonly GraphicsDevice _device;
+        private readonly ResourceSet _resourceSet;
+        private readonly D3D12.Resource _renderTarget;
+        private readonly D3D12.DescriptorHeap _rtvHeap;
+
+        public D3D12HelloTriangleRenderTarget(IntPtr handle, int width, int height)
+        {
+            _device = new GraphicsDevice(false);
+            _resourceSet = new ResourceSet(_device, width, height, new Shader());
+
+            _renderTarget = _device.Device.CreateCommittedResource(new D3D12.HeapProperties(D3D12.HeapType.Default),
+                D3D12.HeapFlags.None,
+                D3D12.ResourceDescription.Texture2D(DXGI.Format.B8G8R8A8_UNorm, width, height),
+                D3D12.ResourceStates.PixelShaderResource);
+
+            
+            var rtvHeapDesc = new D3D12.DescriptorHeapDescription
+            {
+                Type = D3D12.DescriptorHeapType.RenderTargetView,
+                DescriptorCount = 1,
+                Flags = D3D12.DescriptorHeapFlags.None,
+                NodeMask = 0
+            };
+            _rtvHeap = _device.Device.CreateDescriptorHeap(rtvHeapDesc);
+        }
+
+        public void Dispose()
+        {
+            _renderTarget.Dispose();
+            _resourceSet.Dispose();
+            _device.Dispose();
+        }
+
+        public void Render(float ratio)
+        {
+            _resourceSet.Render(ratio, _renderTarget, _rtvHeap.CPUDescriptorHandleForHeapStart);
+            _resourceSet.Fence.WaitForPreviousFrame();
+        }
+    }
+}
---
+++ D3D12HelloTriangleSharp/GraphicsDevice9.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using D3D9 = SharpDX.Direct3D9;
+
+namespace D3D12HelloTriangleSharp
+{
+    public sealed class GraphicsDevice9 : IDisposable
+    {
+        public GraphicsDevice9(int width, int height)
+        {
+            D3DEx = new D3D9.Direct3DEx();
+            Device = new D3D9.DeviceEx(D3DEx, 0, D3D9.DeviceType.Hardware, IntPtr.Zero,
+                D3D9.CreateFlags.Multithreaded | D3D9.CreateFlags.HardwareVertexProcessing,
+                new D3D9.PresentParameters
+                {
+                    BackBufferWidth = 1,
+                    BackBufferHeight = 1,
+                    BackBufferFormat = D3D9.Format.A8R8G8B8,
+                    BackBufferCount = 1,
+                    MultiSampleType = D3D9.MultisampleType.None,
+                    MultiSampleQuality = 0,
+                    SwapEffect = D3D9.SwapEffect.Discard,
+                    DeviceWindowHandle = IntPtr.Zero,
+                    Windowed = true,
+                    EnableAutoDepthStencil = false,
+                    AutoDepthStencilFormat = D3D9.Format.D16,
+                    PresentFlags = D3D9.PresentFlags.None,
+                    FullScreenRefreshRateInHz = 0,
+                    PresentationInterval = D3D9.PresentInterval.Default
+                });
+
+            IntPtr sharedHandle = IntPtr.Zero;
+            RenderTarget = D3D9.Surface.CreateRenderTarget(Device, width, height, D3D9.Format.A8R8G8B8,
+                D3D9.MultisampleType.None, 0, false, ref sharedHandle);
+            RenderTargetSharedHanlde = sharedHandle;
+        }
+
+        public void Dispose()
+        {
+            RenderTarget.Dispose();
+            Device.Dispose();
+            D3DEx.Dispose();
+        }
+
+        public D3D9.Direct3DEx D3DEx { get; }
+        public D3D9.DeviceEx Device { get; }
+        public D3D9.Surface RenderTarget { get; }
+        private IntPtr RenderTargetSharedHanlde { get; }
+    }
+}
